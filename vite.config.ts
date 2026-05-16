468ab8504d88f927399a6eeee6b8156de12014b2
zxznew3-cmyk <zxznew3@gmail.com>
2026-05-16 18:55:42 +0300

    Fixed Netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import cloudflarePlugin from "@cloudflare/vite-plugin";
+
 
 export default defineConfig({
   plugins: [
-    cloudflarePlugin(),
     TanStackRouterVite(),
     react(),
     tsconfigPaths(),
